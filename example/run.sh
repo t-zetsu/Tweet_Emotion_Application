83e2b51a3a1131ee7cab0aa6d81c2db638686e43
Tatsuya Zetsu <74550103+t-zetsu@users.noreply.github.com>
2022-06-07 18:12:24 +0900

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,8 +1,1 @@
-# git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
-# cd mecab-ipadic-neologd
-# /work/mecab-ipadic-neologd/libexec/install-mecab-ipadic-neologd.sh を編集76line sudoを消す
-# ./bin/install-mecab-ipadic-neologd -n
-# echo `mecab-config --dicdir`"/mecab-ipadic-neologd"
-# ./bin/install-mecab-ipadic-neologd -h
-
 python3 emotion_analysis/ml_ask.py
